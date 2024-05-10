9caa7eb943cc4d9f705aedf2d9137bd788a69faa
Tmplinkz <139988794+Tmplinkz@users.noreply.github.com>
2024-05-10 12:02:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Shortner (token system) 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "api.shareus.io")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "b11f986901b8c56f7c4b8ecbafaed2afc1749fef")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_Download_7x/32")
